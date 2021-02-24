6cd9ef76487994710e71d37af1acc9d0a12d1392
AnotherGhost <lb284542@gmail.com>
2021-02-24 16:34:09 +0000

    Rotational Cylinder

--- Assets/Script/RotationalCylinder.cs
+++ Assets/Script/RotationalCylinder.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationCylinder : MonoBehaviour
+public class RotationalCylinder : MonoBehaviour
 {
-    [SerializeField] float speed = 10f;
+    [SerializeField] float speed = 10000f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,7 @@ public class RotationCylinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        //transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        GetComponent<Rigidbody>().AddTorque(new Vector3(1, 1, 1) * Time.deltaTime * speed);
     }
 }
